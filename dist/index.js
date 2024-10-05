22ba087493c613adef6743473b0076eab10ed8bd
Sukhjeet Singh <sukhjeet20002@gmail.com>
2024-10-05 12:54:48 +0530

    build

--- dist/index.js
+++ dist/index.js
@@ -1,34 +1,39 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.compileJavaWithInput = exports.compileJava = exports.compilePyWithInput = exports.compilePy = exports.compileCPPWithInput = exports.compileCPP = exports.init = void 0;
 const fs = require("fs");
 const compileCPPModule = require("./CppModule");
 const PythonModule = require("./PythonModule");
 const JavaModule = require("./JavaModule");
-const compileJs = {
-    init: () => {
-        const dir = "./code";
-        if (!fs.existsSync(dir)) {
-            console.log("Code directory created");
-            fs.mkdirSync("./code");
-        }
-    },
-    compileCPP: (envData, code, cb) => {
-        compileCPPModule.CompileCPP(envData, code, cb);
-    },
-    compileCPPWithInput: (envData, code, input, cb) => {
-        compileCPPModule.CompileCPPWithInput(envData, code, input, cb);
-    },
-    compilePy: (envData, code, cb) => {
-        PythonModule.CompilePyModule(envData, code, cb);
-    },
-    compilePyWithInput: (envData, code, input, cb) => {
-        PythonModule.CompilePyModuleWithInput(envData, code, input, cb);
-    },
-    compileJava: (envData, code, cb) => {
-        JavaModule.JavaModuleCompile(envData, code, cb);
-    },
-    compileJavaWithInput: (envData, code, input, cb) => {
-        JavaModule.JavaModuleCompileWithInput(envData, code, input, cb);
-    },
+const init = () => {
+    const dir = "./code";
+    if (!fs.existsSync(dir)) {
+        console.log("Code directory created");
+        fs.mkdirSync("./code");
+    }
 };
-exports.default = compileJs;
+exports.init = init;
+const compileCPP = (envData, code, cb) => {
+    compileCPPModule.CompileCPP(envData, code, cb);
+};
+exports.compileCPP = compileCPP;
+const compileCPPWithInput = (envData, code, input, cb) => {
+    compileCPPModule.CompileCPPWithInput(envData, code, input, cb);
+};
+exports.compileCPPWithInput = compileCPPWithInput;
+const compilePy = (envData, code, cb) => {
+    PythonModule.CompilePyModule(envData, code, cb);
+};
+exports.compilePy = compilePy;
+const compilePyWithInput = (envData, code, input, cb) => {
+    PythonModule.CompilePyModuleWithInput(envData, code, input, cb);
+};
+exports.compilePyWithInput = compilePyWithInput;
+const compileJava = (envData, code, cb) => {
+    JavaModule.JavaModuleCompile(envData, code, cb);
+};
+exports.compileJava = compileJava;
+const compileJavaWithInput = (envData, code, input, cb) => {
+    JavaModule.JavaModuleCompileWithInput(envData, code, input, cb);
+};
+exports.compileJavaWithInput = compileJavaWithInput;
--- src/index.ts
+++ src/index.ts
@@ -5,47 +5,49 @@ const compileCPPModule = require("./CppModule");
 const PythonModule = require("./PythonModule");
 const JavaModule = require("./JavaModule");
 
-const compileJs = {
-  init: () => {
-    const dir = "./code";
-    if (!fs.existsSync(dir)) {
-      console.log("Code directory created");
-      fs.mkdirSync("./code");
-    }
-  },
-  compileCPP: (envData: EnvData, code: string, cb: Callback) => {
-    compileCPPModule.CompileCPP(envData, code, cb);
-  },
-  compileCPPWithInput: (
-    envData: EnvData,
-    code: string,
-    input: string,
-    cb: Callback
-  ) => {
-    compileCPPModule.CompileCPPWithInput(envData, code, input, cb);
-  },
-  compilePy: (envData: EnvData, code: string, cb: Callback) => {
-    PythonModule.CompilePyModule(envData, code, cb);
-  },
-  compilePyWithInput: (
-    envData: EnvData,
-    code: string,
-    input: string,
-    cb: Callback
-  ) => {
-    PythonModule.CompilePyModuleWithInput(envData, code, input, cb);
-  },
-  compileJava: (envData: EnvData, code: string, cb: Callback) => {
-    JavaModule.JavaModuleCompile(envData, code, cb);
-  },
-  compileJavaWithInput: (
-    envData: EnvData,
-    code: string,
-    input: string,
-    cb: Callback
-  ) => {
-    JavaModule.JavaModuleCompileWithInput(envData, code, input, cb);
-  },
+export const init = () => {
+  const dir = "./code";
+  if (!fs.existsSync(dir)) {
+    console.log("Code directory created");
+    fs.mkdirSync("./code");
+  }
 };
 
-export default compileJs;
+export const compileCPP = (envData: EnvData, code: string, cb: Callback) => {
+  compileCPPModule.CompileCPP(envData, code, cb);
+};
+
+export const compileCPPWithInput = (
+  envData: EnvData,
+  code: string,
+  input: string,
+  cb: Callback
+) => {
+  compileCPPModule.CompileCPPWithInput(envData, code, input, cb);
+};
+
+export const compilePy = (envData: EnvData, code: string, cb: Callback) => {
+  PythonModule.CompilePyModule(envData, code, cb);
+};
+
+export const compilePyWithInput = (
+  envData: EnvData,
+  code: string,
+  input: string,
+  cb: Callback
+) => {
+  PythonModule.CompilePyModuleWithInput(envData, code, input, cb);
+};
+
+export const compileJava = (envData: EnvData, code: string, cb: Callback) => {
+  JavaModule.JavaModuleCompile(envData, code, cb);
+};
+
+export const compileJavaWithInput = (
+  envData: EnvData,
+  code: string,
+  input: string,
+  cb: Callback
+) => {
+  JavaModule.JavaModuleCompileWithInput(envData, code, input, cb);
+};
