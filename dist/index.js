1620c28c5ad39d332c19c87632ec5fd19ea3eecb
Sukhjeet Singh <sukhjeet20002@gmail.com>
2024-10-05 12:42:01 +0530

    build

--- dist/index.js
+++ dist/index.js
@@ -4,33 +4,31 @@ const fs = require("fs");
 const compileCPPModule = require("./CppModule");
 const PythonModule = require("./PythonModule");
 const JavaModule = require("./JavaModule");
-class CompileJs {
-    constructor() {
-        this.init = function () {
-            const dir = "./code";
-            if (!fs.existsSync(dir)) {
-                console.log("Code directory created");
-                fs.mkdirSync("./code");
-            }
-        };
-        this.compileCPP = (envData, code, cb) => {
-            compileCPPModule.CompileCPP(envData, code, cb);
-        };
-        this.compileCPPWithInput = (envData, code, input, cb) => {
-            compileCPPModule.CompileCPPWithInput(envData, code, input, cb);
-        };
-        this.compilePy = (envData, code, cb) => {
-            PythonModule.CompilePyModule(envData, code, cb);
-        };
-        this.compilePyWithInput = (envData, code, input, cb) => {
-            PythonModule.CompilePyModuleWithInput(envData, code, input, cb);
-        };
-        this.compileJava = (envData, code, cb) => {
-            JavaModule.JavaModuleCompile(envData, code, cb);
-        };
-        this.compileJavaWithInput = (envData, code, input, cb) => {
-            JavaModule.JavaModuleCompileWithInput(envData, code, input, cb);
-        };
-    }
-}
-exports.default = CompileJs;
+const compileJs = {
+    init: () => {
+        const dir = "./code";
+        if (!fs.existsSync(dir)) {
+            console.log("Code directory created");
+            fs.mkdirSync("./code");
+        }
+    },
+    compileCPP: (envData, code, cb) => {
+        compileCPPModule.CompileCPP(envData, code, cb);
+    },
+    compileCPPWithInput: (envData, code, input, cb) => {
+        compileCPPModule.CompileCPPWithInput(envData, code, input, cb);
+    },
+    compilePy: (envData, code, cb) => {
+        PythonModule.CompilePyModule(envData, code, cb);
+    },
+    compilePyWithInput: (envData, code, input, cb) => {
+        PythonModule.CompilePyModuleWithInput(envData, code, input, cb);
+    },
+    compileJava: (envData, code, cb) => {
+        JavaModule.JavaModuleCompile(envData, code, cb);
+    },
+    compileJavaWithInput: (envData, code, input, cb) => {
+        JavaModule.JavaModuleCompileWithInput(envData, code, input, cb);
+    },
+};
+exports.default = compileJs;
--- src/index.ts
+++ src/index.ts
@@ -5,45 +5,47 @@ const compileCPPModule = require("./CppModule");
 const PythonModule = require("./PythonModule");
 const JavaModule = require("./JavaModule");
 
-export default class CompileJs {
-  init = function () {
+const compileJs = {
+  init: () => {
     const dir = "./code";
     if (!fs.existsSync(dir)) {
       console.log("Code directory created");
       fs.mkdirSync("./code");
     }
-  };
-  compileCPP = (envData: EnvData, code: string, cb: Callback) => {
+  },
+  compileCPP: (envData: EnvData, code: string, cb: Callback) => {
     compileCPPModule.CompileCPP(envData, code, cb);
-  };
-  compileCPPWithInput = (
+  },
+  compileCPPWithInput: (
     envData: EnvData,
     code: string,
     input: string,
     cb: Callback
   ) => {
     compileCPPModule.CompileCPPWithInput(envData, code, input, cb);
-  };
-  compilePy = (envData: EnvData, code: string, cb: Callback) => {
+  },
+  compilePy: (envData: EnvData, code: string, cb: Callback) => {
     PythonModule.CompilePyModule(envData, code, cb);
-  };
-  compilePyWithInput = (
+  },
+  compilePyWithInput: (
     envData: EnvData,
     code: string,
     input: string,
     cb: Callback
   ) => {
     PythonModule.CompilePyModuleWithInput(envData, code, input, cb);
-  };
-  compileJava = (envData: EnvData, code: string, cb: Callback) => {
+  },
+  compileJava: (envData: EnvData, code: string, cb: Callback) => {
     JavaModule.JavaModuleCompile(envData, code, cb);
-  };
-  compileJavaWithInput = (
+  },
+  compileJavaWithInput: (
     envData: EnvData,
     code: string,
     input: string,
     cb: Callback
   ) => {
     JavaModule.JavaModuleCompileWithInput(envData, code, input, cb);
-  };
-}
+  },
+};
+
+export default compileJs;
